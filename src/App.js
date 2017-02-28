7175e5b0371b08ccb03a4515d2fed35d7ba06e40
Steve Kinney <hello@stevekinney.net>
2017-02-28 10:16:21 -0700

    Implement user authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { auth } from './firebase';
+import UserInfo from './UserInfo';
+import SignIn from './SignIn';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+  }
+
+  componentWillMount() {
+    auth.onAuthStateChanged((user) => {
+      this.setState({ user });
+    });
+  }
+
   render() {
+    const { user } = this.state;
+
     return (
       <div className="App">
         <div className="App-header">
           <h2>Ask the Audience</h2>
         </div>
+        { user ? <UserInfo user={user} /> : <SignIn />}
       </div>
     );
   }

fe282b00491e3303051ea6100e303fa143f4424c
Steve Kinney <hello@stevekinney.net>
2017-02-28 10:09:19 -0700

    Add additional named exports

--- src/firebase.js
+++ src/firebase.js
@@ -14,3 +14,4 @@ export default firebase;
 
 export const database = firebase.database();
 export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();

a415c8d55b0b6de91e746e3eb9c7367c05edd27c
Steve Kinney <hello@stevekinney.net>
2017-02-28 10:01:02 -0700

    Set up the Firebase SDK

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCwjdb6Vhsu04iONjMxCNY-Eo_aCAXoY-c',
+  authDomain: 'ask-the-audience.firebaseapp.com',
+  databaseURL: 'https://ask-the-audience.firebaseio.com',
+  storageBucket: 'ask-the-audience.appspot.com',
+  messagingSenderId: '473436394005'
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
+export const auth = firebase.auth();

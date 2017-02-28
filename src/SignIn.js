6695059b9d5d56c03473226970ae163856d741f3
Steve Kinney <hello@stevekinney.net>
2017-02-28 10:09:28 -0700

    Add SignIn component

---
+++ src/SignIn.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { auth, googleAuthProvider } from './firebase';
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div className="SignIn">
+        <button onClick={() => auth.signInWithPopup(googleAuthProvider)}>
+          Sign In
+        </button>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
